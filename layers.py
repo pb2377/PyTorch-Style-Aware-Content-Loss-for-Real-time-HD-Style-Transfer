b2469f0a0cfe04f4a4030f1ac135e57e1570210e
pb652 <p.boulton@bath.ac.uk>
2021-03-07 22:18:44 +0000

    fix stuff|

--- layers.py
+++ layers.py
@@ -65,7 +65,7 @@ class UpConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, relu=None):
         super(UpConvBlock, self).__init__()
         # self.block = ConvLayer(in_channels, out_channels/, kernel_size, stride, relu=relu)
-        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride)
+        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, bias=False)
         self.instn = nn.InstanceNorm2d(out_channels,  affine=True)
         self.relu = nn.ReLU()
 
