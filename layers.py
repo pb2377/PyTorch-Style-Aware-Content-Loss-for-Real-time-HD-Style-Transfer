17967c3fe02da682648c0c503d074c45266b35e1
pb652 <p.boulton@bath.ac.uk>
2021-03-12 13:05:14 +0000

    remove bias and zero pad unless specified

--- layers.py
+++ layers.py
@@ -69,7 +69,7 @@ class ResidualBlock(nn.Module):
 class UpConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, relu=True):
         super(UpConvBlock, self).__init__()
-        self.block = ConvLayer(in_channels, out_channels, kernel_size, stride, relu=relu, pad=True)
+        self.block = ConvLayer(in_channels, out_channels, kernel_size, stride, relu=relu, pad=False)
 
     def forward(self, x):
         return self.block(F.interpolate(x, scale_factor=2, mode='nearest'))
